2f96e67e6168ff5a4d40153e6d95597030cf1007
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-03-03 09:09:37 -0300

    Fix: package.json

--- condicional/ifelse.js
+++ condicional/ifelse.js
@@ -2,10 +2,15 @@ import leia from "readline-sync";
 
 let ingresso;
 let idade;
+let nome;
 
 ingresso = leia.keyInYN("Você tem ingresso?");
 idade = leia.questionInt("Qual a sua idade?");
 
 if (ingresso && idade >=18){
-    console.log("Pode entrar");
+    console.log("Bem vindo!");
+    nome = leia.question("Qual o seu nome?");
+    console.log(`Olá ${nome}!`);
+}else{
+    console.log("Não pode entrar");
 }
